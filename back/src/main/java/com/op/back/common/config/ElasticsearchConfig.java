6356b64fc0a7c8819a20c8916c98c943fcd21fb4
LeeHuSeong <dlgntjdsla@gmail.com>
2026-01-13 15:00:55 +0900

    Value로 값 받아옴 & docker-compose파일 변경

--- back/src/main/java/com/op/back/common/config/ElasticsearchConfig.java
+++ back/src/main/java/com/op/back/common/config/ElasticsearchConfig.java
@@ -2,6 +2,7 @@ package com.op.back.common.config;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
@@ -11,9 +12,16 @@ import co.elastic.clients.transport.rest_client.RestClientTransport;
 
 @Configuration
 public class ElasticsearchConfig {
+
+        @Value("${elasticsearch.host}")
+        private String host;
+
+        @Value("${elasticsearch.port}")
+        private int port;
+
         @Bean
         public ElasticsearchClient elasticsearchClient() {
-                RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+                RestClient restClient = RestClient.builder(new HttpHost(host, port)).build();
                 ElasticsearchTransport transport =new RestClientTransport(restClient,new JacksonJsonpMapper());
                 return new ElasticsearchClient(transport);
         }
