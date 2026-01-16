a352e14bd9203f06ec8cf7546944c58bb93dc822
LeeHuSeong <dlgntjdsla@gmail.com>
2026-01-16 14:12:11 +0900

    cront 체크용 push2회차

--- back/src/main/java/com/op/back/BackApplication.java
+++ back/src/main/java/com/op/back/BackApplication.java
@@ -3,7 +3,6 @@ package com.op.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//cron 테스트용 push
 @SpringBootApplication
 public class BackApplication {
 
