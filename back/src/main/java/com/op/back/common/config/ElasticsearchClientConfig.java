ebe3c0b004cf349075d1054206447a6751fa6963
LeeHuSeong <dlgntjdsla@gmail.com>
2026-01-23 11:57:16 +0900

    기존 config랑 다르게 host, port제거하고 uris만 사용

---
+++ back/src/main/java/com/op/back/common/config/ElasticsearchClientConfig.java
@@ -0,0 +1,26 @@
+package com.op.back.common.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchClientConfig {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String uri;
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(HttpHost.create(uri)).build();
+        ElasticsearchTransport transport =
+                new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
+}
